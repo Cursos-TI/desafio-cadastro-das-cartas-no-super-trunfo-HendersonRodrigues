91de18d09f80d78f5d37a3199fe6a7224914e061
HendersonRodrigues <126429221+HendersonRodrigues@users.noreply.github.com>
2024-09-07 23:27:12 +0000

    Finalizando função de cadastro das cartas versão 1.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,37 +9,44 @@
     
 int main() {
     char nome_da_cidade [50] = "Acre";
-    char Codigo_da_cidade [4] = "A01";
-    int populacao = 1;
-    int area = 1;
-    int pib = 0;
+    char codigo_da_cidade [4] = "A01";
+    float populacao = 1;
+    float area = 1;
+    float pib = 0;
     int numero_de_pontos_turisticos = 0;
-
+    float densidade;
+    float per_capita;
 
     printf("Entre com o nome da primeira cidade: \n");
     fgets(nome_da_cidade, 50, stdin);                   // Corrige a entrada com espaço
 
     printf("Entre com o codigo da primeira cidade: \n");
-    scanf("%s", &Codigo_da_cidade);
+    scanf("%s", codigo_da_cidade);
   
     printf("Entre com a população da primeira cidade em mil habitantes: \n");
-    scanf("%d", &populacao);
+    scanf("%f", &populacao);
   
     printf("Entre com a área da primeira cidade em km2: \n");
-    scanf("%d", &area);
+    scanf("%f", &area);
  
     printf("Entre com o PIB da primeira cidade em reais: \n");
-    scanf("%d", &pib);
+    scanf("%f", &pib);
 
     printf("Entre com o numero de pontos turisticos da primeira cidade: \n");
     scanf("%d", &numero_de_pontos_turisticos);
 
+    densidade = populacao/area;
+    per_capita = pib/populacao;
+
     printf("A cidade cadastrada foi: %s", nome_da_cidade);
-    printf("O codgio cadastrado foi: %s\n", Codigo_da_cidade);
-    printf("A população cadastrada foi: %d\n", populacao);
-    printf("A area cadastrada foi: %d\n", area);
-    printf("O PIB cadastrado foi: %d\n", pib);
-    printf("O PIB cadastrado foi: %d\n", numero_de_pontos_turisticos);
+    printf("O codgio cadastrado foi: %s\n", codigo_da_cidade);
+    printf("A população cadastrada foi: %.0f\n", populacao);
+    printf("A area cadastrada foi: %.0f\n", area);
+    printf("O PIB cadastrado foi: %.0f\n", pib);
+    printf("O número de pontos turisticos cadastrados foi: %d\n", numero_de_pontos_turisticos);
+    printf("A densidade populacional é: %.2f\n", densidade);
+    printf("A renda per cápita é: %.2f\n", per_capita);
+
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
