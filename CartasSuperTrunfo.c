3adef8c4fd89d0d9cd0a3c7b6caf789390d9b921
HendersonRodrigues <126429221+HendersonRodrigues@users.noreply.github.com>
2024-09-09 21:27:36 +0000

    correções

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidade[][4], float densidade[], float pib[], float area[], float populacao[], float pib_per_capita[]);
-void comparar_cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidade[][4], float densidade[], float pib[], float area[], float populacao[], float pib_per_capita[]);
+void cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidade[][4], float densidade[], float pib[], float area[], float populacao[], float pib_per_capita[], int pontos_turisticos[]);
+void comparar_cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidade[][4], float densidade[], float pib[], float area[], float populacao[], float pib_per_capita[], int pontos_turisticos[]);
 
 int main() {
     int quantas_cidades = 0;
@@ -16,6 +16,7 @@ int main() {
     float pib[32];
     float densidade[32];
     float pib_per_capita[32]; // Array para PIB per capita
+    int pontos_turisticos[32]; // Array para número de pontos turísticos
 
     do {
         printf("\nBem-vindo ao jogo Super Trunfo!\n");
@@ -36,14 +37,23 @@ int main() {
                 scanf("%d", &quantas_cidades);
             }
             
-            cartas(quantas_cidades, nome_da_cidade, codigo_da_cidade, densidade, pib, area, populacao, pib_per_capita);
+            cartas(quantas_cidades, nome_da_cidade, codigo_da_cidade, densidade, pib, area, populacao, pib_per_capita, pontos_turisticos);
             
             // Pergunta se o usuário deseja jogar após criar as cartas
             char jogar;
             printf("Você gostaria de jogar agora? (s/n)\n");
             scanf(" %c", &jogar);
             if (jogar == 's' || jogar == 'S') {
-                comparar_cartas(quantas_cidades, nome_da_cidade, codigo_da_cidade, densidade, pib, area, populacao, pib_per_capita);
+                do {
+                    comparar_cartas(quantas_cidades, nome_da_cidade, codigo_da_cidade, densidade, pib, area, populacao, pib_per_capita, pontos_turisticos);
+                    char jogar_novamente;
+                    printf("Gostaria de jogar outra vez? (s/n)\n");
+                    scanf(" %c", &jogar_novamente);
+                    if (jogar_novamente != 's' && jogar_novamente != 'S') {
+                        printf("Você pode jogar mais tarde. Volte sempre!\n");
+                        break; // Sai do loop se o usuário não quiser jogar novamente
+                    }
+                } while (1); // Loop infinito até o usuário decidir sair
             } else {
                 printf("Você pode jogar mais tarde. Volte sempre!\n");
             }
@@ -64,7 +74,7 @@ int main() {
     return 0;
 }
 
-void cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidade[][4], float densidade[], float pib[], float area[], float populacao[], float pib_per_capita[]) {
+void cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidade[][4], float densidade[], float pib[], float area[], float populacao[], float pib_per_capita[], int pontos_turisticos[]) {
     for (int posicao_matriz = 0; posicao_matriz < quantas_cidades; posicao_matriz++) {
         printf("\nEntre com o nome da cidade: \n");
         scanf("%s", nome_da_cidade[posicao_matriz]);
@@ -81,6 +91,9 @@ void cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidad
         printf("Entre com o PIB da cidade em reais: \n");
         scanf("%f", &pib[posicao_matriz]);
 
+        printf("Entre com o número de pontos turísticos da cidade: \n");
+        scanf("%d", &pontos_turisticos[posicao_matriz]);
+
         densidade[posicao_matriz] = populacao[posicao_matriz] / area[posicao_matriz];
         pib_per_capita[posicao_matriz] = pib[posicao_matriz] / populacao[posicao_matriz]; // Cálculo do PIB per capita
     }
@@ -92,17 +105,22 @@ void cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidad
         printf("O PIB da cidade %s cadastrado foi: %.0f\n", codigo_da_cidade[posicao_matriz], pib[posicao_matriz]);
         printf("A densidade populacional da cidade %s é: %.2f\n", codigo_da_cidade[posicao_matriz], densidade[posicao_matriz]);
         printf("O PIB per capita da cidade %s é: %.2f\n", codigo_da_cidade[posicao_matriz], pib_per_capita[posicao_matriz]);
+        printf("O número de pontos turísticos da cidade %s é: %d\n", codigo_da_cidade[posicao_matriz], pontos_turisticos[posicao_matriz]);
     }
 }
 
-void comparar_cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidade[][4], float densidade[], float pib[], float area[], float populacao[], float pib_per_capita[]) {
+void comparar_cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo_da_cidade[][4], float densidade[], float pib[], float area[], float populacao[], float pib_per_capita[], int pontos_turisticos[]) {
     int carta1, carta2;
     int pontos_carta1 = 0, pontos_carta2 = 0;
 
-    printf("Escolha a primeira carta (0 a %d): ", quantas_cidades - 1);
+    printf("Escolha a primeira carta (1 a %d): ", quantas_cidades);
     scanf("%d", &carta1);
-    printf("Escolha a segunda carta (0 a %d): ", quantas_cidades - 1);
+    printf("Escolha a segunda carta (1 a %d): ", quantas_cidades);
     scanf("%d", &carta2);
+
+    // Ajustar para índice de 0
+    carta1--; 
+    carta2--;
 
     if (carta1 < 0 || carta1 >= quantas_cidades || carta2 < 0 || carta2 >= quantas_cidades) {
         printf("Escolhas inválidas. Tente novamente.\n");
@@ -166,6 +184,17 @@ void comparar_cartas(int quantas_cidades, char nome_da_cidade[][50], char codigo
         pontos_carta2++;
     } else {
         printf("Empate em população: %.2f\n", populacao[carta1]);
+    }
+
+    // Comparação de Pontos Turísticos
+    if (pontos_turisticos[carta1] > pontos_turisticos[carta2]) {
+        printf("A carta %s vence com %d pontos turísticos\n", nome_da_cidade[carta1], pontos_turisticos[carta1]);
+        pontos_carta1++;
+    } else if (pontos_turisticos[carta1] < pontos_turisticos[carta2]) {
+        printf("A carta %s vence com %d pontos turísticos\n", nome_da_cidade[carta2], pontos_turisticos[carta2]);
+        pontos_carta2++;
+    } else {
+        printf("Empate em pontos turísticos: %d\n", pontos_turisticos[carta1]);
     }
 
     // Exibição do placar final
